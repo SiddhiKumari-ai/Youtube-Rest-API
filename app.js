be10672fe55e7bc95e5ec15b436a43da57134c3f
SiddhiKumari-ai <aryanverma15030@gmail.com>
2026-05-08 20:26:04 +0530

    app.js change

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const fileUpload = require('express-fileupload')
 const connectDB = async()=>{
     try
     {
-        console.log("Mongo URL:", process.env.MONGODB_URL)
+        console.log("ALL ENV KEYS:", Object.keys(process.env))
         await mongoose.connect(process.env.MONGODB_URL)
         console.log("Connected with Database...")
         
